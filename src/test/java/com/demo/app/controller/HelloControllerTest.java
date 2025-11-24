3de8280a1df3f5ba8fc95054d2197bce3fd8e502
Ritesh Gangthade <ritesh.gangthade@multigenesys.com>
2025-11-24 23:15:10 +0530

    Initial commit

---
+++ src/test/java/com/demo/app/controller/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.demo.app.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello from CI/CD demo app!"));
+    }
+
+    @Test
+    public void testHelloEndpointReturnsString() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"));
+    }
+}
